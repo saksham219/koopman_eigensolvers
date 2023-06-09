2a3cef46c1e4ba8e931413641c86d42fdfda558a
Saksham Malhotra <saksham.malhotra@tum.de>
2023-06-09 16:35:02 +0200

    add function to get extended eigenfunction matrix

--- koopmaneigen/koopman_eigensolver.py
+++ koopmaneigen/koopman_eigensolver.py
@@ -71,7 +71,66 @@ class KoopmanEigenSolvers:
 
         return eigfunc_extended
 
-            
+
+    def eigenfunction_right(self, x:np.ndarray, right_eigvecs=None):        
+        """Get the eigenfunctions using the right eigenvector matrix
+
+        Args:
+            x (np.ndarray): _description_
+            right_eigvecs (_type_, optional): matrix of right eigenvectors to use . Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """        
+        
+        if right_eigvecs == None:
+            right_eigvecs = self.right_koopman_eigvecs
+
+        dictionary_eval = self.dict_transform(x)
+        eigfunc_matrix = np.linalg.lstsq(right_eigvecs, dictionary_eval.T, rcond=1e-8)[0]
+        return eigfunc_matrix.T
+
+    def reconstruct_observable(self, x:np.ndarray, eigfunc_matrix:np.ndarray):
+        """Reconstruct an observable using eigenfucions
+
+        Args:
+            x (np.ndarray): observable values
+            eigfunc_matrix (np.ndarray): _description_
+        """
+        coeff = np.linalg.lstsq(eigfunc_matrix , x, rcond=1e-8)[0]
+        
+        recons_obs = eigfunc_matrix@coeff
+        return recons_obs
+
+
+    def extended_eigfunction_matrix(self, x:np.ndarray, eigvector_idx = [0,1], 
+                                    exp_pairs=[(0,1), (1,0), (1,1)]):
+        """Get extended set of eigenfunctions using two left eigenvectors and their powers
+
+        Args:
+            x (np.ndarray): _description_
+            left_eigvec_i (_type_): _description_
+            left_eigvec_j (_type_): _description_
+            exp_pairs (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """        
+
+        eigfunc_matrix = np.empty((0, x.shape[0])) 
+
+        assert len(eigvector_idx) == 2
+
+        for (i,j) in exp_pairs:
+                if i + j ==0:
+                    continue
+                    
+                new_eigfunc = self.extend_eigenfunctions(x, eigenvector_indexes=eigvector_idx, 
+                                                            pow_i=i, pow_j=j)
+                eigfunc_matrix = np.vstack((eigfunc_matrix, new_eigfunc))
+
+        eigfunc_matrix = eigfunc_matrix.T
+        return eigfunc_matrix
 
 
 
